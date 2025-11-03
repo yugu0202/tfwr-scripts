f0dd5e5e211627506986ba5aaea7abb3bacbc4e7
yugu0202 <applerin1119@gmail.com>
2025-11-04 03:10:00 +0900

    迷路の攻略アルゴリズムの修正

--- bush.py
+++ bush.py
@@ -17,20 +17,31 @@ def main(line, _e, _h, _p):
 			break
 		first_bush_line += variables.plant_rules[v]
 	bush_workers = variables.plant_rules[Entities.Bush]
-	start_pos_y = first_bush_line
-	start_pos_x = (line - first_bush_line) * bush_workers
-	for _i in range(line - start_pos_y):
-		move(South)
-	for _i in range(start_pos_x):
-		move(East)
-	
-	for _i in range((bush_workers + 1) / 2 - 1):
-		move(East)
-		move(North)
-	plant(Entities.Bush)
+	center = ((bush_workers + 1) / 2 - 1)
+	start_pos_y = first_bush_line + center
+	start_pos_x = (line - first_bush_line) * bush_workers + center
 	while True:
-		if can_harvest():
-			cost = get_cost(Entities.Hedge)[Items.Weird_Substance]
-			use_item(Items.Weird_Substance, cost * bush_workers)
-			maze_runner.run()
+		diff_y = get_pos_y() - start_pos_y
+		diff_x = get_pos_x() - start_pos_x
+		if diff_y < 0:
+			for _i in range(abs(diff_y)):
+				move(North)
+		else:
+			for _i in range(diff_y):
+				move(South)
+		
+		if diff_x < 0:
+			for _i in range(abs(diff_x)):
+				move(East)
+		else:
+			for _i in range(diff_x):
+				move(West)
+		
+		plant(Entities.Bush)
+		while True:
+			if can_harvest():
+				cost = get_cost(Entities.Hedge)[Items.Weird_Substance]
+				use_item(Items.Weird_Substance, cost * bush_workers)
+				maze_runner.run()
+				break
 	
--- maze_runner.py
+++ maze_runner.py
@@ -7,6 +7,13 @@ def run():
 		if get_entity_type() == Entities.Treasure:
 			harvest()
 			break
+		
+		# 左向きのインデックス
+		left_index = (index + 3) % 4
+		if can_move(direction[left_index]):
+			move(direction[left_index])
+			index = left_index
+			continue
 
 		if can_move(direction[index]):
 			move(direction[index])
